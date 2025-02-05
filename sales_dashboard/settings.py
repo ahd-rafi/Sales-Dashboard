d08d54bf5f7c7b1c087b9947883fd9113ef48a02
ahd-rafi <ahdrafi1486@gmail.com>
2025-02-05 07:20:16 +0530

    render

--- sales_dashboard/settings.py
+++ sales_dashboard/settings.py
@@ -51,8 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+
+# make sure the below lines also included
+STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'sales_dashboard.urls'
 
 TEMPLATES = [
